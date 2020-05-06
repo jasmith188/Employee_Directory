78eb658d3473a1206712259b9230ffbc35e7badf
Jimmy Smith <jasmith.188@gmail.com>
2020-05-05 22:57:21 -0400

    adding render to main.js

--- employeedirectory/src/components/Main.js
+++ employeedirectory/src/components/Main.js
@@ -1,11 +1,35 @@
 import React from "react";
-
+import API from "../utils/API";
 
 class Main extends React.Component {
 
+        state = {
+            employees: []
+        }
+
+        componentDidMount() {
+            API.getEmployees()
+                .then( data => {
+                    console.log(data);
+                    this.setState({
+                        employees: data.data.results
+                    })
+                    
+                })
+                .catch(err => console.log(err));
+                
+        }
+
+        render() {
+
+            return  (
+                
+            )
 
 
 
+
+        }
 
 
 
